48da36dd11d146e634b5e36a378d994cc49117f1
theory-of-soul <phoenix-12@ya.ru>
2020-06-25 11:25:23 +0800

    added placeCardList component with snapshot test

---
+++ src/components/place-card-list/place-card-list.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import PropTypes from 'prop-types';
+import PlaceCard from "../place-card/place-card";
+
+class PlaceCardList extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeCard: null
+    };
+    this.handleHoverCard = this.handleHoverCard.bind(this);
+  }
+
+  handleHoverCard(activeCard) {
+    this.setState({activeCard});
+  }
+
+  render() {
+    const {placeList} = this.props;
+
+    return (
+      <div className="cities__places-list places__list tabs__content">
+        {
+          placeList.map((place) => {
+            return (
+              <PlaceCard
+                key={place.id}
+                handleHover={this.handleHoverCard}
+                place={place}
+              />
+            );
+          })
+        }
+      </div>
+    );
+  }
+}
+
+PlaceCardList.propTypes = {
+  placeList: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        mark: PropTypes.string,
+        img: PropTypes.string.isRequired,
+        value: PropTypes.number.isRequired,
+        time: PropTypes.string.isRequired,
+        isInBookmark: PropTypes.bool.isRequired,
+        rating: PropTypes.number.isRequired,
+        name: PropTypes.string.isRequired,
+        type: PropTypes.string.isRequired,
+      }).isRequired
+  ).isRequired
+};
+
+export default PlaceCardList;
---
+++ src/components/place-card-list/place-card-list.test.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import renderer from 'react-test-renderer';
+import PlaceCardList from "./place-card-list";
+
+const placeList = [{
+  id: 0,
+  mark: `premium`,
+  img: `img/apartment-01.jpg`,
+  value: 120,
+  time: `night`,
+  isInBookmark: true,
+  rating: 5,
+  name: `luxurious apartment at great location`,
+  type: `Apartment`
+}, {
+  id: 1,
+  img: `img/apartment-02.jpg`,
+  value: 200,
+  time: `week`,
+  isInBookmark: false,
+  rating: 0,
+  name: `Beautiful & luxurious`,
+  type: `Hotel`
+}];
+
+describe(`PlaceCardList component snapshot tests`, () => {
+  it(`PlaceCardList component show two places`, () => {
+    const tree = renderer
+      .create(<PlaceCardList placeList={placeList}/>)
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
