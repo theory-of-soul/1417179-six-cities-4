202befbc8d699b27430d891571f6a628bbdd60de
theory-of-soul <phoenix-12@ya.ru>
2020-06-25 11:09:48 +0800

    added placeCard component with snapshot tests

---
+++ src/components/place-card/place-card.test.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import renderer from 'react-test-renderer';
+import PlaceCard from "./place-card";
+
+const placeList = [{
+  mark: `premium`,
+  img: `img/apartment-01.jpg`,
+  value: 120,
+  time: `night`,
+  isInBookmark: true,
+  rating: 5,
+  name: `luxurious apartment at great location`,
+  type: `Apartment`
+}, {
+  img: `img/apartment-02.jpg`,
+  value: 200,
+  time: `week`,
+  isInBookmark: false,
+  rating: 0,
+  name: `Beautiful & luxurious`,
+  type: `Hotel`
+}];
+
+describe(`PlaceCard component snapshot tests`, () => {
+  it(`PlaceCard component show place with a mark and in bookmark`, () => {
+    const tree = renderer
+      .create(<PlaceCard place={placeList[0]} handleHover={() => {}}/>)
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  it(`PlaceCard component show place without mark and not in bookmark`, () => {
+    const tree = renderer
+      .create(<PlaceCard place={placeList[1]} handleHover={() => {}}/>)
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
