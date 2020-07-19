ceedbb705ee3dfe41f0e537bb4927cf9f4968eda
theory-of-soul <phoenix-12@ya.ru>
2020-07-19 20:42:26 +0800

    added api

--- src/api.js
+++ src/api.js
@@ -6,7 +6,7 @@ const Error = {
 
 export const createAPI = (onUnauthorized) => {
   const api = axios.create({
-    baseURL: `https://4.react.pages.academy/guess-melody`,
+    baseURL: `https://4.react.pages.academy/six-cities`,
     timeout: 5000,
     withCredentials: true,
   });
