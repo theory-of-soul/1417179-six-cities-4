dbff245f54040fd9a9b4ab8406f5aedd22759f48
theory-of-soul <phoenix-12@ya.ru>
2020-07-20 21:10:14 +0800

    added place-property component with tests

--- src/components/place-property/place-property.jsx
+++ src/components/place-property/place-property.jsx
@@ -1,39 +1,44 @@
 import React from "react";
+import PropTypes from 'prop-types';
 
-const PlaceProperty = () => {
+const PlaceProperty = (props) => {
+  const {
+    images,
+    title,
+    description,
+    isPremium,
+    type,
+    rating,
+    rooms,
+    guests,
+    price,
+    goods,
+    host: {
+      icon,
+      name,
+      isSuper,
+    }
+  } = props;
   return (
     <section className="property">
       <div className="property__gallery-container container">
         <div className="property__gallery">
-          <div className="property__image-wrapper">
-            <img className="property__image" src="img/room.jpg" alt="Photo studio"/>
-          </div>
-          <div className="property__image-wrapper">
-            <img className="property__image" src="img/apartment-01.jpg" alt="Photo studio"/>
-          </div>
-          <div className="property__image-wrapper">
-            <img className="property__image" src="img/apartment-02.jpg" alt="Photo studio"/>
-          </div>
-          <div className="property__image-wrapper">
-            <img className="property__image" src="img/apartment-03.jpg" alt="Photo studio"/>
-          </div>
-          <div className="property__image-wrapper">
-            <img className="property__image" src="img/studio-01.jpg" alt="Photo studio"/>
-          </div>
-          <div className="property__image-wrapper">
-            <img className="property__image" src="img/apartment-01.jpg" alt="Photo studio"/>
-          </div>
+          {
+            images.map((img, i) => (
+              <div className="property__image-wrapper" key={i}>
+                <img className="property__image" src={img} alt={title} />
+              </div>
+            ))
+          }
         </div>
       </div>
       <div className="property__container container">
         <div className="property__wrapper">
           <div className="property__mark">
-            <span>Premium</span>
+            {isPremium && <span>Premium</span>}
           </div>
           <div className="property__name-wrapper">
-            <h1 className="property__name">
-              Beautiful &amp; luxurious studio at great location
-            </h1>
+            <h1 className="property__name">{title}</h1>
             <button className="property__bookmark-button button" type="button">
               <svg className="property__bookmark-icon" width="31" height="33">
                 <use xlinkHref="#icon-bookmark"></use>
@@ -43,81 +48,45 @@ const PlaceProperty = () => {
           </div>
           <div className="property__rating rating">
             <div className="property__stars rating__stars">
-              <span style={{width: `80%`}}/>
+              <span style={{width: `${rating * 20}%`}}/>
               <span className="visually-hidden">Rating</span>
             </div>
-            <span className="property__rating-value rating__value">4.8</span>
+            <span className="property__rating-value rating__value">{rating}</span>
           </div>
           <ul className="property__features">
             <li className="property__feature property__feature--entire">
-              Apartment
+              {type}
             </li>
             <li className="property__feature property__feature--bedrooms">
-              3 Bedrooms
+              {rooms} Bedrooms
             </li>
             <li className="property__feature property__feature--adults">
-              Max 4 adults
+              Max {guests} adults
             </li>
           </ul>
           <div className="property__price">
-            <b className="property__price-value">&euro;120</b>
+            <b className="property__price-value">&euro;{price}</b>
             <span className="property__price-text">&nbsp;night</span>
           </div>
           <div className="property__inside">
             <h2 className="property__inside-title">What&apos;s inside</h2>
             <ul className="property__inside-list">
-              <li className="property__inside-item">
-                Wi-Fi
-              </li>
-              <li className="property__inside-item">
-                Washing machine
-              </li>
-              <li className="property__inside-item">
-                Towels
-              </li>
-              <li className="property__inside-item">
-                Heating
-              </li>
-              <li className="property__inside-item">
-                Coffee machine
-              </li>
-              <li className="property__inside-item">
-                Baby seat
-              </li>
-              <li className="property__inside-item">
-                Kitchen
-              </li>
-              <li className="property__inside-item">
-                Dishwasher
-              </li>
-              <li className="property__inside-item">
-                Cabel TV
-              </li>
-              <li className="property__inside-item">
-                Fridge
-              </li>
+              {goods.map((good, i) => (<li className="property__inside-item" key={i}>{good}</li>))}
             </ul>
           </div>
           <div className="property__host">
             <h2 className="property__host-title">Meet the host</h2>
             <div className="property__host-user user">
-              <div className="property__avatar-wrapper property__avatar-wrapper--pro user__avatar-wrapper">
-                <img className="property__avatar user__avatar" src="img/avatar-angelina.jpg" width="74" height="74"
+              <div className={
+                `property__avatar-wrapper user__avatar-wrapper ${isSuper ? `property__avatar-wrapper--pro` : ``}`
+              }>
+                <img className="property__avatar user__avatar" src={icon} width="74" height="74"
                   alt="Host avatar"/>
               </div>
-              <span className="property__user-name">
-                    Angelina
-              </span>
+              <span className="property__user-name">{name}</span>
             </div>
             <div className="property__description">
-              <p className="property__text">
-                A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The
-                building is green and from 18th century.
-              </p>
-              <p className="property__text">
-                An independent House, strategically located between Rembrand Square and National Opera, but where the
-                bustle of the city comes to rest in this alley flowery and colorful.
-              </p>
+              {description}
             </div>
           </div>
           <section className="property__reviews reviews">
@@ -208,6 +177,34 @@ const PlaceProperty = () => {
   );
 };
 
-PlaceProperty.propTypes = {};
+const typeAccommodation = {
+  APARTMENT: `apartment`,
+  ROOM: `room`,
+  HOUSE: `house`,
+  HOTEL: `hotel`
+};
+
+PlaceProperty.propTypes = {
+  images: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  isPremium: PropTypes.bool.isRequired,
+  type: PropTypes.oneOf([
+    typeAccommodation.APARTMENT,
+    typeAccommodation.ROOM,
+    typeAccommodation.HOTEL,
+    typeAccommodation.HOUSE,
+  ]).isRequired,
+  rating: PropTypes.number.isRequired,
+  rooms: PropTypes.number.isRequired,
+  guests: PropTypes.number.isRequired,
+  price: PropTypes.number.isRequired,
+  goods: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
+  host: PropTypes.shape({
+    icon: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    isSuper: PropTypes.bool.isRequired,
+  })
+};
 
 export default React.memo(PlaceProperty);
